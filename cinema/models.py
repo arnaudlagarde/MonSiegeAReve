c88fb85186d2ed8ae0ddea0dd92ad5e2a38e5d9b
arnaudlagarde <xx-momo@hotmail.fr>
2023-07-19 11:49:31 +0200

    Initial commit

---
+++ cinema/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=12.50)
+
+    def __str__(self):
+        return self.title
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    available_seats = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.date} {self.time}"
+
+class SpecialSession(models.Model):
+    movie = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=20)
+
+    def __str__(self):
+        return f"{self.movie} - {self.date} {self.time}"
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    seats = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.session.movie.title} - {self.seats} seat(s)"
+
