3a7deb18ed96170db0aa5417491506ac6f738036
arnaudlagarde <xx-momo@hotmail.fr>
2023-07-20 19:33:19 +0200

    enhanced Navbar

--- frontend/cinema_front/src/components/Navbar.js
+++ frontend/cinema_front/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const CustomNavbar = () => {
-
     const authToken = localStorage.getItem('authToken'); // Get the authentication token from localStorage
 
     const handleLogout = () => {
@@ -13,36 +14,45 @@ const CustomNavbar = () => {
         localStorage.removeItem('username');
         localStorage.removeItem('is_staff');
         window.location.reload();
-      };
-    
+    };
 
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand as={Link} to="/">Cinéma "Mon Siège à rêve"</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="lg" style={{ padding: '15px 30px', justifyContent: 'space-between' }}>
+            <Navbar.Brand as={Link} to="/">
+                Mon Siège à rêve
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-nav" />
             <Navbar.Collapse id="navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link as={Link} to="/">Accueil</Nav.Link>
-                    <Nav.Link as={Link} to="/reservation">Réserver des places</Nav.Link>
-                    {!authToken && ( // Show the following links only when the user is not logged in
-                    <>
-                        <Nav.Link as={Link} to="/signup">
-                            S'inscrire
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/signin">
-                            Se connecter
-                        </Nav.Link>
-                    </>
-                    )}
-                    {authToken && ( // Show the following link only when the user is logged in
-                        <Nav.Link as={Link} to="/" onClick={handleLogout}>
-                            Se déconnecter
-                        </Nav.Link>
-                    )}
-                    <Nav.Link as={Link} to="/moviesessions">Movie Sessions</Nav.Link>
+                    <Nav.Link as={Link} to="/">
+                        Accueil
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/reservation">
+                        Réserver des places
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/moviesessions">
+                        Movie Sessions
+                    </Nav.Link>
                     <Nav.Link as={Link} to="/add-movie">
-            Ajouter un film
-          </Nav.Link>
+                        Ajouter un film
+                    </Nav.Link>
+                </Nav>
+                <Nav>
+                    {!authToken ? (
+                        <>
+                            <Nav.Link as={Link} to="/signup">
+                                S'inscrire
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/signin">
+                                Se connecter
+                            </Nav.Link>
+                        </>
+                    ) : (
+                        <Button variant="dark" size="sm" onClick={handleLogout} style={{ display: 'flex', alignItems: 'center' }}>
+                            <FontAwesomeIcon icon={faUser} style={{ color: 'white', marginRight: '5px' }} />
+                            Se déconnecter
+                        </Button>
+                    )}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
