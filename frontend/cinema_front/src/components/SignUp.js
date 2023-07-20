828a0476f2e9d9119eb695a5bde378b591cd6b3c
arnaudlagarde <xx-momo@hotmail.fr>
2023-07-20 13:37:14 +0200

    Signup enhanced

--- frontend/cinema_front/src/components/SignUp.js
+++ frontend/cinema_front/src/components/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from '../api'; // Import the updated api.js file
+import { Button, Toast } from 'react-bootstrap';
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
@@ -8,9 +9,17 @@ const SignUp = () => {
     isAdmin: 'no', // Par défaut, l'utilisateur n'est pas un administrateur
   });
 
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [showErrorToast, setShowErrorToast] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCloseToasts = () => {
+    setShowSuccessToast(false);
+    setShowErrorToast(false);
   };
 
   const handleSubmit = async (e) => {
@@ -33,33 +42,58 @@ const SignUp = () => {
       localStorage.setItem('authToken', token);
 
       console.log('User registered:', response.data);
-      // You can redirect to a different page here after successful registration
+
+      // Show the success toast
+      setShowSuccessToast(true);
     } catch (error) {
       console.error('Error registering user:', error);
+
+      // Show the error toast
+      setShowErrorToast(true);
     }
   };
 
   return (
-    <div>
-      <h2>Créer un compte</h2>
+    <div className="container">
+      <h2 className="my-4">Créer un compte</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nom d'utilisateur :</label>
-          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        <div className="mb-3">
+          <label className="form-label">Nom d'utilisateur :</label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange} className="form-control" />
         </div>
-        <div>
-          <label>Mot de passe :</label>
-          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <div className="mb-3">
+          <label className="form-label">Mot de passe :</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} className="form-control" />
         </div>
-        <div>
-          <label>Administrateur :</label>
-          <select name="isAdmin" value={formData.isAdmin} onChange={handleChange}>
+        <div className="mb-3">
+          <label className="form-label">Administrateur :</label>
+          <select name="isAdmin" value={formData.isAdmin} onChange={handleChange} className="form-select">
             <option value="yes">Oui</option>
             <option value="no">Non</option>
           </select>
         </div>
-        <button type="submit">S'inscrire</button>
+        <button type="submit" className="btn btn-primary">S'inscrire</button>
       </form>
+
+      {/* Success Toast */}
+      <Toast show={showSuccessToast} onClose={handleCloseToasts} delay={3000} autohide>
+        <Toast.Header closeButton={false}>
+          <strong className="me-auto">Success</strong>
+        </Toast.Header>
+        <Toast.Body>
+          Your registration was successful.
+        </Toast.Body>
+      </Toast>
+
+      {/* Error Toast */}
+      <Toast show={showErrorToast} onClose={handleCloseToasts} delay={3000} autohide bg="danger" text="white">
+        <Toast.Header closeButton={false}>
+          <strong className="me-auto">Error</strong>
+        </Toast.Header>
+        <Toast.Body>
+          There was an error with your registration. Please try again.
+        </Toast.Body>
+      </Toast>
     </div>
   );
 };
